4284b06f3d5f9bbb2c40da7b2fddb315b2e86976
Bjorn Kristinsson <bjorn@bjornkri.com>
2017-01-24 11:32:50 +0100

    Move bjcp api to subdir

--- wb/wb/urls.py
+++ wb/wb/urls.py
@@ -19,8 +19,8 @@ from rest_framework import routers
 from bjcp import views
 
 router = routers.DefaultRouter()
-router.register(r'categories', views.CategoryViewSet)
-router.register(r'styles', views.SubCategoryViewSet)
+router.register(r'bjcp/categories', views.CategoryViewSet)
+router.register(r'bjcp/styles', views.SubCategoryViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
