58fdbbd6f43d0086b93bc6953d0808b96a6f7727
Bjorn Kristinsson <bjorn@bjornkri.com>
2017-01-24 23:06:48 +0100

    Add eslint

---
+++ wb/js_src/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => (
+  <div>
+    <h1>Coming soon</h1>
+    <p>In the meantime, here&rsqo;s a free <a href="/api/">BJCP API</a></p>
+  </div>
+);
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
